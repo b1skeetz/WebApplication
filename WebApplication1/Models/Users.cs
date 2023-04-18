adcb3960b833146f916e8157700fc9438b1416c1
Damir Kadyrzhanov <damirk120404@gmail.com>
2023-04-19 01:55:18 +0600

    first commit

---
+++ WebApplication1/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ WebApplication1/Services/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class ApplicationContext : DbContext
+    {
+        DbSet<Users> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=aspnet;Username=postgres;Password=mercy07");
+        }
+    }
+}
